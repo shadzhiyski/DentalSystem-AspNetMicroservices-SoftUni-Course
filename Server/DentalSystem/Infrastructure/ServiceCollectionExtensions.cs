56c4406081e0955822cd03af777107e4f8343f75
stoian2662 <stoian2662@gmail.com>
2021-10-04 23:42:25 +0300

    ServiceCollectionExtensions added.

---
+++ Server/DentalSystem/Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,127 @@
+﻿namespace DentalSystem.Infrastructure
+{
+    using System;
+    using System.Reflection;
+    using System.Text;
+    using AutoMapper;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
+    using Microsoft.Data.SqlClient;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.IdentityModel.Tokens;
+    using Models;
+    using Services.Identity;
+
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddWebService<TDbContext>(
+            this IServiceCollection services,
+            IConfiguration configuration,
+            bool databaseHealthChecks = true,
+            bool messagingHealthChecks = true)
+            where TDbContext : DbContext
+        {
+            services
+                .AddDatabase<TDbContext>(configuration)
+                .AddApplicationSettings(configuration)
+                .AddTokenAuthentication(configuration)
+                .AddHealth(configuration, databaseHealthChecks, messagingHealthChecks)
+                .AddAutoMapperProfile(Assembly.GetCallingAssembly())
+                .AddControllers();
+
+            return services;
+        }
+
+        public static IServiceCollection AddDatabase<TDbContext>(
+            this IServiceCollection services,
+            IConfiguration configuration)
+            where TDbContext : DbContext
+            => services
+                .AddScoped<DbContext, TDbContext>()
+                .AddDbContext<TDbContext>(options => options
+                    .UseSqlServer(
+                        configuration.GetDefaultConnectionString(),
+                        sqlOptions => sqlOptions
+                            .EnableRetryOnFailure(
+                                maxRetryCount: 10,
+                                maxRetryDelay: TimeSpan.FromSeconds(30),
+                                errorNumbersToAdd: null)));
+
+        public static IServiceCollection AddApplicationSettings(
+            this IServiceCollection services,
+            IConfiguration configuration)
+            => services
+                .Configure<ApplicationSettings>(
+                    configuration.GetSection(nameof(ApplicationSettings)),
+                    config => config.BindNonPublicProperties = true);
+
+        public static IServiceCollection AddTokenAuthentication(
+            this IServiceCollection services,
+            IConfiguration configuration,
+            JwtBearerEvents events = null)
+        {
+            services
+                .AddHttpContextAccessor()
+                .AddScoped<ICurrentUserService, CurrentUserService>();
+
+            var secret = configuration
+                .GetSection(nameof(ApplicationSettings))
+                .GetValue<string>(nameof(ApplicationSettings.Secret));
+
+            var key = Encoding.ASCII.GetBytes(secret);
+
+            services
+                .AddAuthentication(authentication =>
+                {
+                    authentication.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    authentication.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(bearer =>
+                {
+                    bearer.RequireHttpsMetadata = false;
+                    bearer.SaveToken = true;
+                    bearer.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+
+                    if (events != null)
+                    {
+                        bearer.Events = events;
+                    }
+                });
+
+            return services;
+        }
+
+        public static IServiceCollection AddAutoMapperProfile(
+            this IServiceCollection services,
+            Assembly assembly)
+            => services
+                .AddAutoMapper(
+                    (_, config) => config
+                        .AddProfile(new MappingProfile(assembly)),
+                    Array.Empty<Assembly>());
+
+        public static IServiceCollection AddHealth(
+            this IServiceCollection services,
+            IConfiguration configuration,
+            bool databaseHealthChecks = true,
+            bool messagingHealthChecks = true)
+        {
+            var healthChecks = services.AddHealthChecks();
+
+            if (databaseHealthChecks)
+            {
+                healthChecks
+                    .AddSqlServer(configuration.GetDefaultConnectionString());
+            }
+
+            return services;
+        }
+    }
+}
