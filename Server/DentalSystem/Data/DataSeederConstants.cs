e90e3811650c2bb54c6aa2a8a1e643c4602cd221
stoian2662 <stoian2662@gmail.com>
2021-10-21 22:27:05 +0300

    DataSeederConstants added.

---
+++ Server/DentalSystem/Data/DataSeederConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace DentalSystem.Data
+{
+    public class DataSeederConstants
+    {
+        public const string DefaultUserId = "6be0140d-91db-46e3-adc0-bba8839239c6";
+        public const string DefaultUserPassword = "dentalcare12!";
+    }
+}
