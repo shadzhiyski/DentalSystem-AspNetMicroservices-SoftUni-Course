92fcff24ade5f7967116c81908252f7489110c29
stoian2662 <stoian2662@gmail.com>
2021-10-21 22:25:17 +0300

    TokenGeneratorService and IdentityService added.

---
+++ Server/DentalSystem.Identity/Services/Identity/IIdentityService.cs
@@ -0,0 +1,16 @@
+﻿namespace DentalSystem.Identity.Services.Identity
+{
+    using System.Threading.Tasks;
+    using DentalSystem.Services;
+    using Data.Models;
+    using Models.Identity;
+
+    public interface IIdentityService
+    {
+        Task<Result<User>> Register(UserInputModel userInput);
+
+        Task<Result<UserOutputModel>> Login(UserInputModel userInput);
+
+        Task<Result> ChangePassword(string userId, ChangePasswordInputModel changePasswordInput);
+    }
+}
