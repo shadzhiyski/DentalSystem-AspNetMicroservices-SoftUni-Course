de173c7237ce44a366ec9fa1aaf4d01039fe97a5
stoian2662 <stoian2662@gmail.com>
2021-10-31 20:44:29 +0200

    AddMessaging added.

--- Server/DentalSystem/Infrastructure/ServiceCollectionExtensions.cs
+++ Server/DentalSystem/Infrastructure/ServiceCollectionExtensions.cs
@@ -4,6 +4,12 @@
     using System.Reflection;
     using System.Text;
     using AutoMapper;
+    using GreenPipes;
+    using DentalSystem.Messages;
+    using DentalSystem.Services.Messages;
+    using Hangfire;
+    using Hangfire.SqlServer;
+    using MassTransit;
     using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Microsoft.Data.SqlClient;
     using Microsoft.EntityFrameworkCore;
@@ -121,7 +127,113 @@
                     .AddSqlServer(configuration.GetDefaultConnectionString());
             }
 
+            if (messagingHealthChecks)
+            {
+                var messageQueueSettings = GetMessageQueueSettings(configuration);
+
+                var messageQueueConnectionString =
+                    $"amqp://{messageQueueSettings.UserName}:{messageQueueSettings.Password}@{messageQueueSettings.Host}/";
+
+                healthChecks
+                    .AddRabbitMQ(rabbitConnectionString: messageQueueConnectionString);
+            }
+
             return services;
+        }
+
+        public static IServiceCollection AddMessaging(
+            this IServiceCollection services,
+            IConfiguration configuration,
+            bool usePolling = true,
+            params Type[] consumers)
+        {
+            services
+                .AddTransient<IPublisher, Publisher>()
+                .AddTransient<IMessageService, MessageService>();
+
+            var messageQueueSettings = GetMessageQueueSettings(configuration);
+
+            services
+                .AddMassTransit(mt =>
+                {
+                    consumers.ForEach(consumer => mt.AddConsumer(consumer));
+
+                    mt.AddBus(context => Bus.Factory.CreateUsingRabbitMq(rmq =>
+                    {
+                        rmq.Host(messageQueueSettings.Host, host =>
+                        {
+                            host.Username(messageQueueSettings.UserName);
+                            host.Password(messageQueueSettings.Password);
+                        });
+
+                        rmq.UseHealthCheck(context);
+
+                        consumers.ForEach(consumer => rmq.ReceiveEndpoint(consumer.FullName, endpoint =>
+                        {
+                            endpoint.PrefetchCount = 6;
+                            endpoint.UseMessageRetry(retry => retry.Interval(5, 200));
+
+                            endpoint.ConfigureConsumer(context, consumer);
+                        }));
+                    }));
+                })
+                .AddMassTransitHostedService();
+
+            if (usePolling)
+            {
+                CreateHangfireDatabase(configuration);
+
+                services
+                    .AddHangfire(config => config
+                        .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                        .UseSimpleAssemblyNameTypeSerializer()
+                        .UseRecommendedSerializerSettings()
+                        .UseSqlServerStorage(
+                            configuration.GetCronJobsConnectionString(),
+                            new SqlServerStorageOptions
+                            {
+                                CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                                SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                                QueuePollInterval = TimeSpan.Zero,
+                                UseRecommendedIsolationLevel = true,
+                                DisableGlobalLocks = true
+                            }));
+
+                services.AddHangfireServer();
+
+                services.AddHostedService<MessagesHostedService>();
+            }
+
+            return services;
+        }
+
+        private static MessageQueueSettings GetMessageQueueSettings(IConfiguration configuration)
+        {
+            var settings = configuration.GetSection(nameof(MessageQueueSettings));
+
+            return new MessageQueueSettings(
+                settings.GetValue<string>(nameof(MessageQueueSettings.Host)),
+                settings.GetValue<string>(nameof(MessageQueueSettings.UserName)),
+                settings.GetValue<string>(nameof(MessageQueueSettings.Password)));
+        }
+
+        private static void CreateHangfireDatabase(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetCronJobsConnectionString();
+
+            var dbName = connectionString
+                .Split(";")[1]
+                .Split("=")[1];
+
+            using var connection = new SqlConnection(connectionString.Replace(dbName, "master"));
+
+            connection.Open();
+
+            using var command = new SqlCommand(
+                $"IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'{dbName}') create database [{dbName}];",
+                connection);
+
+            command.ExecuteNonQuery();
         }
     }
 }
