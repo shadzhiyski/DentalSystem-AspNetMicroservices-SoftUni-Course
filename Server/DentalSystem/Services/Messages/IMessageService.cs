d695700f9f19f99ff98d11c1e3a4b16bae63b192
stoian2662 <stoian2662@gmail.com>
2021-10-31 20:30:09 +0200

    MessageService added.

---
+++ Server/DentalSystem/Services/Messages/IMessageService.cs
@@ -0,0 +1,12 @@
+﻿namespace DentalSystem.Services.Messages
+{
+    using System.Threading.Tasks;
+
+    public interface IMessageService
+    {
+        Task<bool> IsDuplicated(
+            object messageData,
+            string propertyFilter,
+            object identifier);
+    }
+}
