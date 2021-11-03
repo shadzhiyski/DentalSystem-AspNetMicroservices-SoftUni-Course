ca34c1845f5eada3168c9bb556bd3e175b52c5cb
stoian2662 <stoian2662@gmail.com>
2021-11-03 21:56:33 +0200

    ReferenceId added to Dentist.

--- Server/DentalSystem.Scheduling/Data/Configurations/DentistConfiguration.cs
+++ Server/DentalSystem.Scheduling/Data/Configurations/DentistConfiguration.cs
@@ -3,12 +3,16 @@ namespace DentalSystem.Scheduling.Data.Configurations
     using Models;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using DentalSystem.Data;
 
     public class DentistConfiguration : IEntityTypeConfiguration<Dentist>
     {
         public void Configure(EntityTypeBuilder<Dentist> builder)
         {
             builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.ReferenceId).HasValueGenerator<ReferenceIdGenerator>();
+            builder.HasIndex(e => e.ReferenceId).IsUnique();
 
             builder.Property(e => e.UserId).IsRequired();
             builder.HasIndex(e => e.UserId).IsUnique(true);
