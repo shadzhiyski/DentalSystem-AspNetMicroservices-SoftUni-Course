38baaf301b7260e3e8c18e4b9c544446ef7c1b0d
stoian2662 <stoian2662@gmail.com>
2021-11-13 20:26:54 +0200

    AuthorizePatient attribute added.

---
+++ Server/DentalSystem/Infrastructure/AuthorizePatientAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace DentalSystem.Infrastructure
+{
+    using Microsoft.AspNetCore.Authorization;
+    using static Constants;
+
+    public class AuthorizePatientAttribute : AuthorizeAttribute
+    {
+        public AuthorizePatientAttribute() => this.Roles = PatientRoleName;
+    }
+}
