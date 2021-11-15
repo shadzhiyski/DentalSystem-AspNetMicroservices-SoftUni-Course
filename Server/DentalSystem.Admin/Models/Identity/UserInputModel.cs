84164a195f0cde9da31be7ae3ae6b95613839b65
stoian2662 <stoian2662@gmail.com>
2021-11-15 23:36:06 +0200

    IIdentityService and user models added.

---
+++ Server/DentalSystem.Admin/Models/Identity/UserInputModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DentalSystem.Admin.Models.Identity
+{
+    using DentalSystem.Models;
+
+    public class UserInputModel : IMapFrom<LoginFormModel>
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
