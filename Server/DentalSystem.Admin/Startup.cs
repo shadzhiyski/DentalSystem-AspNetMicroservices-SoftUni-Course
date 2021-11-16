892281937610d35bf8c6636871cf5b500107c463
stoian2662 <stoian2662@gmail.com>
2021-11-16 23:13:30 +0200

    IIdentityService Refit and Policy configuration added.

--- Server/DentalSystem.Admin/Startup.cs
+++ Server/DentalSystem.Admin/Startup.cs
@@ -1,23 +1,36 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+
 
 namespace DentalSystem.Admin
 {
+    using DentalSystem.Admin.Services;
+    using DentalSystem.Admin.Services.Identity;
+    using DentalSystem.Infrastructure;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Refit;
+
     public class Startup
     {
         public Startup(IConfiguration configuration) => Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
-        public void ConfigureServices(IServiceCollection services) => services.AddControllersWithViews();
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var serviceEndpoints = this.Configuration
+                .GetSection(nameof(ServiceEndpoints))
+                .Get<ServiceEndpoints>(config => config.BindNonPublicProperties = true);
+
+            services
+                .AddControllersWithViews();
+
+            services
+                .AddRefitClient<IIdentityService>()
+                .WithConfiguration(serviceEndpoints.Identity);
+        }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) =>
             (env.IsDevelopment()
---
+++ Server/DentalSystem/Services/Identity/ICurrentTokenService.cs
@@ -0,0 +1,9 @@
+﻿namespace DentalSystem.Services.Identity
+{
+    public interface ICurrentTokenService
+    {
+        string Get();
+
+        void Set(string token);
+    }
+}
