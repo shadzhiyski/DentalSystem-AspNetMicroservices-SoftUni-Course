dec99f0f627120a234b4994b1dfa23dcab7af3b6
stoian2662 <stoian2662@gmail.com>
2021-11-18 23:23:04 +0200

    Login page added in DentslSystem.Admin.

---
+++ Server/DentalSystem.Admin/Controllers/IdentityController.cs
@@ -0,0 +1,58 @@
+﻿namespace DentalSystem.Admin.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+    using AutoMapper;
+    using DentalSystem.Infrastructure;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Models.Identity;
+    using Services.Identity;
+
+    using static DentalSystem.Infrastructure.InfrastructureConstants;
+
+    public class IdentityController : AdministrationController
+    {
+        private readonly IIdentityService identityService;
+        private readonly IMapper mapper;
+
+        public IdentityController(
+            IIdentityService identityService,
+            IMapper mapper)
+        {
+            this.identityService = identityService;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginFormModel model)
+            => await this.Handle(
+                async () =>
+                {
+                    var result = await this.identityService
+                        .Login(this.mapper.Map<UserInputModel>(model));
+
+                    this.Response.Cookies.Append(
+                        AuthenticationCookieName,
+                        result.Token,
+                        new CookieOptions
+                        {
+                            HttpOnly = true,
+                            Secure = false,
+                            MaxAge = TimeSpan.FromDays(1)
+                        });
+                },
+                success: RedirectToAction(nameof(SchedulingController.Index), "Scheduling"),
+                failure: View("../Home/Index", model));
+
+        [AuthorizeAdministrator]
+        public IActionResult Logout()
+        {
+            this.Response.Cookies.Delete(AuthenticationCookieName);
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
