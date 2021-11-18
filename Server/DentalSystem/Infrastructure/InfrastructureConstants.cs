a5e24bf7c673b5f4d908ba3867f39d3bce150a4f
stoian2662 <stoian2662@gmail.com>
2021-11-18 23:23:49 +0200

    Authorization constants added.

--- Server/DentalSystem/Infrastructure/InfrastructureConstants.cs
+++ Server/DentalSystem/Infrastructure/InfrastructureConstants.cs
@@ -2,6 +2,9 @@
 {
     public class InfrastructureConstants
     {
+        public const string AuthenticationCookieName = "Authentication";
+        public const string AuthorizationHeaderName = "Authorization";
+        public const string AuthorizationHeaderValuePrefix = "Bearer";
         public const string UserReferenceIdLabel = "ReferenceId";
     }
 }
