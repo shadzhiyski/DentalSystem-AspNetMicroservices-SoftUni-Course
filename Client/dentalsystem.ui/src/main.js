711f7550997f1ee7fa76567221dd803a2cb80892
stoian2662 <stoian2662@gmail.com>
2021-11-21 22:57:00 +0200

    Common components grouped.

--- Client/dentalsystem.ui/src/main.js
+++ Client/dentalsystem.ui/src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
+import router from './common/router'
+import store from './common/store'
+import vuetify from './common/plugins/vuetify'
 
 Vue.config.productionTip = false
 
