7b9572f3bfc27733698853d18faf33af3e3e99be
stoian2662 <stoian2662@gmail.com>
2021-11-22 23:28:00 +0200

    Carousel added.

---
+++ Client/dentalsystem.ui/src/common/components/Carousel.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-card
+    elevation="24"
+    max-width="1024"
+    class="mx-auto"
+  >
+    <v-system-bar lights-out></v-system-bar>
+    <v-carousel
+      :continuous="true"
+      :cycle="cycle"
+      :show-arrows="true"
+      hide-delimiter-background
+      delimiter-icon="mdi-minus"
+      height="300"
+    >
+      <v-carousel-item
+        v-for="(slide, i) in slides"
+        :key="i"
+      >
+        <v-sheet
+          :color="colors[i % colors.length]"
+          height="100%"
+          tile
+        >
+          <v-row
+            class="fill-height"
+            align="center"
+            justify="center"
+          >
+            <div class="display-3">
+              {{ slide }}
+            </div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+    <v-list two-line>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>Supported Dental Services</v-list-item-title>
+          <v-list-item-subtitle>List of all supported dental services</v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-switch
+            v-model="cycle"
+            label="Cycle Slides"
+            inset
+          ></v-switch>
+        </v-list-item-action>
+      </v-list-item>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    slides: Array
+  },
+  data() {
+    return {
+      cycle: true,
+      colors: [
+        'green',
+        'secondary',
+        'yellow darken-4',
+        'red lighten-2',
+        'orange darken-1'
+      ]
+    }
+  }
+}
+</script>
