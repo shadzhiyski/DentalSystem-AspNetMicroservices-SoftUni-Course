ada44f9039f3028ec08b0ae8a4323ac5d56ff00c
stoian2662 <stoian2662@gmail.com>
2022-04-06 20:22:52 +0300

    API key validation on Identity service applied.

--- Client/dentalsystem.ui/src/identity/plugins/auth-web-service.js
+++ Client/dentalsystem.ui/src/identity/plugins/auth-web-service.js
@@ -3,9 +3,10 @@
 import { AxiosWebServiceBuilder } from '../../common/plugins/axios-web-service-builder'
 
 let config = {
-  baseURL: process.env.VUE_APP_IDENTITY_BASE_URL
-  // timeout: 60 * 1000, // Timeout
-  // withCredentials: true, // Check cross-site Access-Control
+  baseURL: process.env.VUE_APP_IDENTITY_BASE_URL,
+  headers: {
+    "apikey": process.env.VUE_APP_IDENTITY_API_KEY
+  }
 };
 
 export default new AxiosWebServiceBuilder()
